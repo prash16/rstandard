db854491339b5a474979ef1c1fce29b42bdffa12
Oliver Pringle <ojapringle@gmail.com>
2016-11-23 09:01:44 -0800

    ok will it work now?!

--- R/se.R
+++ R/se.R
@@ -1,1 +1,20 @@
-standard_error <- function(x) standard_deviation(x)/sqrt(length(x))
+#' Calculate standard error of the mean
+#' This function calculates standard error
+#'
+#' @param x vector
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#'
+#' standard_error(c(1,2,3,4))
+#'
+standard_error <- function(x) {
+	n <- length(x)
+	mean = sum(x) / n
+	ssq <- sum((x-mean)^2)
+	stddev = sqrt(ssq/n)
+	stderr = stddev/(n**0.5)
+	return(stderr)
+}
