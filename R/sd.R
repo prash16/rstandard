3fee79d51c166db8d85b98b910d20cbd99e76e24
Prash Medirattaa <prash16@gmail.com>
2016-11-23 08:12:25 -0800

    second commit

--- R/sd.R
+++ R/sd.R
@@ -1,3 +1,16 @@
+#' Calculate standard deviation
+#'
+#' This function calculates standard deviation
+#'
+#' @param x vector
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#'
+#' standard_deviation(c(1,2,3,4))
+#'
 standard_deviation <- function(x) {
 	n <- length(x)
 	mean = sum(x) / n
