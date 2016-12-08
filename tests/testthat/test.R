bd4ba9b6433aa423ef8fc24dbde787d3eacd47a8
Prash Medirattaa <prash16@gmail.com>
2016-12-08 12:09:50 -0800

    changes

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -3,10 +3,8 @@ library(rstandard)
 context("standard error function")
 
 test_that("Correct Standard deviation results",{
-	standard_deviation(c(1,1,1))=0
-	standard_deviation(-5,-5)= 0
-	standard_deviation(c())
-	standard_deviation("hello")
+	expect_equal(standard_deviation(c(1,1,1)),0)
+	#expect_equal(standard_deviation(-5,-5)= 0)
+	#expect_equal(standard_deviation(c()))
+	#expect_equal(standard_deviation("hello"))
 })
-
-
