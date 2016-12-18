fcd98d993e1dd2d2f70b0cf543094db37b4f878c
Prash Medirattaa <prash16@gmail.com>
2016-12-18 12:50:33 -0800

    changes

--- R/rstandard-package.r
+++
@@ -1,7 +0,0 @@
-#' rstandard: This package contains function for statistics
-#'
-#'In statistics it is important calcualte standard deviation
-#'
-#' @name rstandard
-#' @docType package
-NULL
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -4,7 +4,8 @@ context("standard error function")
 
 test_that("Correct Standard deviation results",{
 	expect_equal(standard_deviation(c(1,1,1)),0)
+	expect_equal(standard_deviation(c(-1,-1,-1)),0)
 	expect_equal(standard_deviation(c(-5,-5)),0)
 	expect_equal(standard_deviation(c()),NaN)
-	#expect_equal(standard_deviation("hello"),"invalid 'type' (character) of argument")
+	#expect_match(standard_deviation("hello"),invalid)
 })
