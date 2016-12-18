9665449f5d0dadb459332d0aa085577875b5d1a4
Prash Medirattaa <prash16@gmail.com>
2016-12-18 13:05:57 -0800

    added test function

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -7,5 +7,5 @@ test_that("Correct Standard deviation results",{
 	expect_equal(standard_deviation(c(-1,-1,-1)),0)
 	expect_equal(standard_deviation(c(-5,-5)),0)
 	expect_equal(standard_deviation(c()),NaN)
-	#expect_match(standard_deviation("hello"),invalid)
+	expect_error(standard_deviation("hello"),)
 })
